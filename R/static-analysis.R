cb4a8b3c9333c9544e65ea71399cf97ec4dc80cd
Thomas Mailund <mailund@birc.au.dk>
2018-03-22 14:50:43 +0100

    Annotating expressions with variables that are potentially assigned to.
    Resolves #25 but some documentation for how the heurstics work is really needed.

--- R/static-analysis.R
+++ R/static-analysis.R
@@ -92,10 +92,24 @@ collect_assigned_symbols_in_function <- function(fun, topdown = list()) {
 
 ## Functions annotating expressions #########################################
 
+# FIXME: maybe move to a misc file and export...
+collect_from_args <- function(expr, attribute,
+                              condition = function(expr) TRUE
+                              ) {
+    collected <- list()
+    args <- rlang::call_args(expr)
+    for (a in args) {
+        if (condition(a))
+            collected <- c(collected, attr(a, attribute))
+    }
+    collected
+}
+
 annotate_assigned_symbols_callback <- function(expr, next_cb, ...) {
 
-    #bottomup <- merge_bottomup(bottomup)
-    bottomup <- list() # FIXME: GET FROM CALL ARGS
+    locals <- collect_from_args(expr, "assigned_symbols") %>%
+        unlist %>% unique
+    if (is.null(locals)) locals <- character()
 
     # This function is installed to be called on assignments and
     # on for-loops (where there is an implicit assignment to the
@@ -103,11 +117,11 @@ annotate_assigned_symbols_callback <- function(expr, next_cb, ...) {
     if (expr[[1]] == "<-") {
         if (rlang::is_symbol(expr[[2]])) {
             local_var <- as.character(expr[[2]])
-            locals <- c(local_var, bottomup$locals)
+            locals <- c(local_var, locals)
         }
     } else if (expr[[1]] == "for") {
         local_var <- as.character(expr[[2]])
-        locals <- c(local_var, bottomup$locals)
+        locals <- c(local_var, locals)
     } else {
         stop("Unexpected function call!")
     }
@@ -117,9 +131,72 @@ annotate_assigned_symbols_callback <- function(expr, next_cb, ...) {
 }
 
 propagate_assigned_symbols_callback <- function(expr, ...) {
-    42 # FIXME
+
+    # The rules for when we are assigning to a local variable are a bit
+    # complicated. For control structures, we can assume that assignments will
+    # be to the local scope. People can change the implementation of these so it
+    # isn't, but then they are only hurting themselves and deserve the extra
+    # pain we can give them. For other call arguments, it gets a little more
+    # complicated. With standard-evaluation, if we have an arrow assignment in a
+    # function argument, then the assignment happens in the calling scope. So we
+    # will assume this happens unless we are handling cases we know have NSE,
+    # such as `with`. If an assignment is inside a block, however, we will
+    # assume that NSE *is* in play, by default, and not consider it a local
+    # assignment.
+
+    # FIXME: Document these heuristics
+    # FIXME: Make a handle so people can guide these heuristics, e.g. tell
+    # when arguments are evaluated in the calling scope and when they are
+    # evaluated in another and assignments won't affect this scope.
+    call_name <- as.character(expr[[1]])
+    if (call_name %in% c("{", "if", "for", "while", "repeat")) {
+        condition <- function(expr) TRUE
+    } else if (call_name == "with") {
+        condition <- function(expr) FALSE
+    } else {
+        condition <- function(expr) {
+            rlang::is_lang(expr) && expr[[1]] == "<-"
+        }
+    }
+    locals <- collect_from_args(expr, "assigned_symbols", condition) %>%
+        unlist %>% unique
+
+    if (is.null(locals)) locals <- character()
+    attr(expr, "assigned_symbols") <- locals
+    expr
 }
 
-annotate_assigned_symbols_callbacks <- analysis_callbacks() %>%
-    add_call_callback(`<-`, collection_callback) %>%
-    add_call_callback(`for`, collection_callback)
+annotate_assigned_symbols_callbacks <- rewrite_callbacks() %>%
+    with_call_callback(propagate_assigned_symbols_callback) %>%
+    add_call_callback(`<-`, annotate_assigned_symbols_callback) %>%
+    add_call_callback(`for`, annotate_assigned_symbols_callback)
+
+
+
+#' Extracts all the symbols that appear on the left-hand side of an
+#' assignment and annotate expressions with those potentially in scope.
+#'
+#' Since R does not require that we declare local variables, and since
+#' the variables that are assigned to a local scope depend on the runtime
+#' execution of functions, we cannot determine with any certainty which
+#' variables will be assigned to in any given scope at any given program
+#' point. So the best we can do is figure out which variables are
+#' *potentially* assigned to. Which is what this function does.
+#'
+#' The [collect_assigned_symbols_in_function()] function reformats the collected
+#' data into a character vector, removes duplications, and remove the
+#' formal parameters of the function from the list, so those are not considered
+#' local variables (rather, they are considered formals and presumably handled
+#' elsewhere as such).
+#'
+#' @param fn      The function whose body we should analyse
+#'
+#' @return A function who's expressions are annotated with potentially
+#'    local variables.
+#'
+#' @export
+annotate_assigned_symbols_in_function <- function(fn) {
+    depth_first_rewrite_function(
+        fn, annotate_assigned_symbols_callbacks
+    )
+}
--- tests/testthat/test-static-analysis.R
+++ tests/testthat/test-static-analysis.R
@@ -1,5 +1,6 @@
 context("Static analysis")
 
+## Collection ###########################################################
 test_that("we can collect the symbols in a simple expression", {
     expr <- quote(42)
     res <- collect_assigned_symbols_in_expression(expr, env = environment())
@@ -90,4 +91,93 @@ test_that("we don't collect symbols we know are in a different scope", {
     }
     res <- collect_assigned_symbols_in_function(f)
     expect_equal(res, c("y"))
+})
+
+
+## Annotation ###########################################################
+test_that("we can annotate with the symbols in a simple function", {
+    f <- function() {
+        42
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), character(0))
+
+    f <- function() {
+        x <- 42
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), "x")
+
+    f <- function() {
+        x <- 42
+        y <- 24
+        x + y
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), c("x", "y"))
+
+    f <- function(x = 42) {
+        y <- 24
+        x + y
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), "y")
+
+    # If we analyse the full function, we might not want to consider
+    # formal parameters as local variables, but inside the *body*
+    # of this function, we do assign to `x`, so we include it in
+    # the annotation.
+    f <- function(x = 42) {
+        x <- 42
+        y <- 24
+        x + y
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), c("x", "y"))
+
+    # we shouldn't include duplications
+    f <- function(x = 42) {
+        y <- x
+        x <- 42
+        y <- x + 2
+        x + y
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), c("y", "x"))
+})
+
+test_that("we can annotate with symbols when there are for-loops", {
+    f <- function(n) {
+        for (i in 1:n) i
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), "i")
+
+    f <- function(n) {
+        x <- 0
+        for (i in 1:n) {
+            x <- x + i
+        }
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), c("x", "i"))
+})
+
+test_that("we don't annotate with symbols we know are in a different scope", {
+    f <- function(df) {
+        with(df, z <- x + y)
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), character())
+
+    f <- function(x) {
+        y <- 2 * x
+        function(x) {
+            z <- 2 * x
+        }
+    }
+    res <- annotate_assigned_symbols_in_function(f)
+    expect_equal(attr(body(res), "assigned_symbols"), c("y"))
+    nested_body <- body(res)[[3]][[3]]
+    expect_equal(attr(nested_body, "assigned_symbols"), c("z"))
 })
